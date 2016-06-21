b2376b749af7f163b0fd8fb53f03bd7ebca9a99f
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-06-20 23:06:51 -0300

    Fix migrations

--- eventex/core/migrations/0010_course.py
+++ eventex/core/migrations/0010_course.py
@@ -16,7 +16,14 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Course',
             fields=[
-                ('talk_ptr', models.OneToOneField(auto_created=True, on_delete=django.db.models.deletion.CASCADE, parent_link=True, primary_key=True, serialize=False, to='core.Talk')),
+                ('talk_ptr', models.OneToOneField(
+                    auto_created=True,
+                    on_delete=django.db.models.deletion.CASCADE,
+                    parent_link=True,
+                    primary_key=True,
+                    serialize=False,
+                    to='core.Talk'
+                )),
                 ('slots', models.IntegerField()),
             ],
             options={
