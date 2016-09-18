33a27a1bebc04e5590b09ead4914d900f5efbd9d
RonaldTheodoro <ronaldtheodoro@gmail.com>
2016-09-17 21:31:52 -0300

    use git cdn

--- eventex/wsgi.py
+++ eventex/wsgi.py
@@ -1,8 +1,14 @@
 import os
 from dj_static import Cling
-
+from decouple import config
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventex.settings")
 
-application = Cling(get_wsgi_application())
+if config('DEBUG', default=False, cast=bool):
+    # Use Cling and staticfiles when DEBUG is true
+    application = Cling(get_wsgi_application())
+else:
+    # Use git cdn when DEBUG is false
+    application = get_wsgi_application()
